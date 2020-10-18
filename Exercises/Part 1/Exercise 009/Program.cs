b83404127355ac247c922ccffba13bd1367c12ea
Jake Reddy <Serathian@outlook.com>
2020-10-18 12:09:13 +0300

    18,10,20

--- Exercises/Part 1/Exercise 009/Program.cs
+++ Exercises/Part 1/Exercise 009/Program.cs
@@ -7,7 +7,13 @@ namespace exercise_09
         public static void Main(string[] args)
         {
             // Write your code here:
-
+            Console.WriteLine("Hey How are you?");
+            string replyOne = Console.ReadLine();
+            Console.WriteLine("Ok then, Why is that?");
+            string replyTwo = Console.ReadLine();
+            Console.WriteLine("Interesting");
+            
         }
     }
 }
+ 
