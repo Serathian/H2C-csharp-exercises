355fc5b248108e6c01b14d0393956f9def9b815b
Jake Reddy <Serathian@outlook.com>
2020-10-18 11:44:54 +0300

    18,10,20

--- Exercises/Part 1/Exercise 006/Program.cs
+++ Exercises/Part 1/Exercise 006/Program.cs
@@ -8,7 +8,8 @@ namespace exercise_06
         {
             Console.WriteLine("Give input!");
             // Write your code here:
-            
+            Console.WriteLine(Console.ReadLine());
+
         }
     }
 }
