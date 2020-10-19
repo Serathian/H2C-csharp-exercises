0d59b950d8152c33523c621491187711c065f6e0
Jake Reddy <Serathian@outlook.com>
2020-10-19 12:55:24 +0300

    19,10,20

--- Exercises/Part 1/Exercise 032/Program.cs
+++ Exercises/Part 1/Exercise 032/Program.cs
@@ -7,6 +7,12 @@ namespace exercise_32
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Tell me the password!");
+            string passWord = Console.ReadLine();
+            if (passWord = "Mellon"){
+            Console.WriteLine("Welcome friend!");}
+            else{
+            Console.WriteLine("they've got a cave troll!");}
 
     }
   }
