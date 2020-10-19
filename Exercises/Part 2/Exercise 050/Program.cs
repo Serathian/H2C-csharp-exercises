83a7c29d8e543eefb9073b7ef3f25b1b1190213d
Jake Reddy <Serathian@outlook.com>
2020-10-19 16:56:32 +0300

    19,10,20

--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -7,12 +7,17 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
- 
+      Console.WriteLine("How many times?");
+            int x = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < x; i++){
+            PrintPhrase();
+            }
     }
 
     // Write your method here:
     public static void PrintPhrase() {
-
+            Console.WriteLine("In a hole in the ground there lived a method");
+                return;
     }
   }
 }
