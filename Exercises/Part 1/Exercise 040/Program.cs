58b6be021c471f938d6aea5522ab06db22bd07e7
Jake Reddy <Serathian@outlook.com>
2020-10-19 13:40:06 +0300

    19,10,20

--- Exercises/Part 1/Exercise 040/Program.cs
+++ Exercises/Part 1/Exercise 040/Program.cs
@@ -8,6 +8,22 @@ namespace exercise_40
     {
 
       // Write your code here:
+      int sum = 0;
+      int number = 1;
+      int tally = 0;
+      int totalSum = 0;
+      
+      while(number != sum){
+                
+      Console.WriteLine("Give me a number");
+            number = Convert.ToInt32(Console.ReadLine());
+                
+                if(number != 0){
+                totalSum= totalSum + number;
+                tally++;}
+        }
+            Console.WriteLine($"Total sum of number: {totalSum}\n" +
+                $"Total amount of numbers: {tally}");
     }
   }
 }
