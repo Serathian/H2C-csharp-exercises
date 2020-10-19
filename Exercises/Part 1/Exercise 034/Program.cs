0e30b5e68a6ddf962767232dc3d303ab7a9af66e
Jake Reddy <Serathian@outlook.com>
2020-10-19 13:09:55 +0300

    19,10,20

--- Exercises/Part 1/Exercise 034/Program.cs
+++ Exercises/Part 1/Exercise 034/Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace exercise_34
 {
   class Program
   {
+       
     public static void Main(string[] args)
     {
       // Write your code here:
+      bool userStatus = true;
 
+            while(userStatus = true){
+            
+            Console.WriteLine("Do you want to continue?");
+            string reply = Console.ReadLine();
+                if (reply == "no"){
+                    userStatus = false; 
+                    Environment.Exit(1);
+                }
+                else{
+                   
+                }
+                
+            }
     }
   }
 }
