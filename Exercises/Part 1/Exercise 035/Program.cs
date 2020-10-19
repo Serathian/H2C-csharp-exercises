5e2cae5b79dc8778b02539b515006ad219bc9bfd
Jake Reddy <Serathian@outlook.com>
2020-10-19 13:14:57 +0300

    19,10,20

--- Exercises/Part 1/Exercise 035/Program.cs
+++ Exercises/Part 1/Exercise 035/Program.cs
@@ -7,7 +7,22 @@ namespace exercise_35
     public static void Main(string[] args)
     {
       // Write your code here:
+      bool correctNum = false;
 
+            while(correctNum == false){
+            
+            Console.WriteLine("Try and guess my number...");
+            int numberGuess = Convert.ToInt32(Console.ReadLine());
+                if (numberGuess == 69){
+                    Console.WriteLine("Good job! thats correct!");
+                    correctNum = true; 
+                    Environment.Exit(1);
+                }
+                else if(numberGuess < 69){
+                Console.WriteLine("Guess a little higher");}
+                else{
+                Console.WriteLine("Guess a little lower");}
+                }
     }
   }
 }
