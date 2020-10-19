f2a772b73e9b56757d05092eaf727f9eb61b0e09
Jake Reddy <Serathian@outlook.com>
2020-10-19 11:06:47 +0300

    19,10,20

--- Exercises/Part 1/Exercise 022/Program.cs
+++ Exercises/Part 1/Exercise 022/Program.cs
@@ -8,7 +8,13 @@ namespace exercise_22
     {
 
       // Write your code here:
-
+      Console.WriteLine("Please enter 2 numbers, Seperated by spaces.");
+      var replyNumbers = Console.ReadLine();
+            var data = replyNumbers.Split();
+            double numberOne = Convert.ToInt32(data[0]);
+            double numberTwo = Convert.ToInt32(data[1]);
+            double numberThree = Convert.ToInt32(data[2]);
+        Console.WriteLine("The average is: {0}",(numberOne + numberTwo + numberThree)/ data.Length);
     }
   }
 }
