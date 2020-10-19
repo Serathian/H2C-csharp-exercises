21f261180036391f288d5a9a42dd272837f3ee1c
Jake Reddy <Serathian@outlook.com>
2020-10-19 11:01:46 +0300

    19,10,20

--- Exercises/Part 1/Exercise 019/Program.cs
+++ Exercises/Part 1/Exercise 019/Program.cs
@@ -8,7 +8,13 @@ namespace exercise_19
     {
 
       // Write your code here:
+        Console.WriteLine("Please enter 2 numbers, Seperated by spaces.");
+      var replyNumbers = Console.ReadLine();
+            var data = replyNumbers.Split();
+            int numberOne = Convert.ToInt32(data[0]);
+            int numberTwo = Convert.ToInt32(data[1]);
 
+        Console.WriteLine("{0} + {1} = {2}", numberOne, numberTwo, numberOne + numberTwo);
     }
   }
 }
