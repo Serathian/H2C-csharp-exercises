5c31eac32f86e1f992d3428c0e87f128fb714b73
Jake Reddy <Serathian@outlook.com>
2020-10-19 12:20:29 +0300

    19,10,20

--- Exercises/Part 1/Exercise 027/Program.cs
+++ Exercises/Part 1/Exercise 027/Program.cs
@@ -7,7 +7,14 @@ namespace exercise_27
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      Console.WriteLine("Give a number: ");
+      int number = Convert.ToInt32(Console.ReadLine());
+            if (number%2 == 0){
+                Console.WriteLine("Number is even");
+            }
+            else{
+            Console.WriteLine("Number is odd");
+                    }
     }
   }
 }
