6674bcb3fd5475dec3a8a32db070a694b1c2f765
Jake Reddy <Serathian@outlook.com>
2020-10-19 10:35:01 +0300

    19,10,20

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -8,7 +8,36 @@ namespace exercise_15
     {
 
       // Write your code here:
+      Console.WriteLine("Give me a string");
+           string replyOne = Console.ReadLine();
+            Console.WriteLine("Give me a integer" );
+            int replyTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give me a double" );
+            double replyThree = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Give me a boolean" );
+            string replyFour = Console.ReadLine().ToLower();
 
+            string replyBool = "null";
+
+            if (replyFour == "true")
+                {
+            replyBool = "True";
+            }
+            else if (replyFour == "false")
+                {
+            replyBool = "Fasle";
+                    }
+            else{
+            replyBool = "reply not valid";
+            }
+
+
+            Console.WriteLine(
+                $"Your string: {replyOne} \n" + 
+                $"Your integer: {replyTwo} \n" +
+                $"Your double: {replyThree} \n" +
+                $"Your boolean: {replyBool} \n"
+                );
     }
   }
 }
