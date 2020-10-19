47bcaaf59c721f9cd15419ca6ff18b2ae6d5c948
Jake Reddy <Serathian@outlook.com>
2020-10-19 11:18:20 +0300

    19,10,20

--- Exercises/Part 1/Exercise 024/Program.cs
+++ Exercises/Part 1/Exercise 024/Program.cs
@@ -8,7 +8,16 @@ namespace exercise_24
     {
 
       // Write your code here:
-      
+      Console.WriteLine("Your speed? enter a number..");
+      int speed = Convert.ToInt32(Console.ReadLine());
+            if (speed > 120){
+                Console.WriteLine("Speeding!");
+            }
+            else{
+                Console.WriteLine("Driving safe :)");
+            }
+           
+
     }
   }
 }
