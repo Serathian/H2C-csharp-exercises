5aed7d293e12ea071267778de84f5d0714156353
Jake Reddy <Serathian@outlook.com>
2020-10-19 12:49:02 +0300

    19,10,20

--- Exercises/Part 1/Exercise 031/Program.cs
+++ Exercises/Part 1/Exercise 031/Program.cs
@@ -7,7 +7,21 @@ namespace exercise_31
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Please enter 2 numbers, Seperated by spaces.");
+      var replyNumbers = Console.ReadLine();
+            var data = replyNumbers.Split();
+            double numberOne = Convert.ToInt32(data[0]);
+            double numberTwo = Convert.ToInt32(data[1]);
 
+        if (numberOne > numberTwo){
+            Console.WriteLine($"The larger number is {numberOne}");
+            }
+        else if (numberTwo > numberOne){
+            Console.WriteLine($"The larger number is {numberTwo}");
+            }
+        else {
+            Console.WriteLine("They are equal!");
+            }
     }
   }
 }
