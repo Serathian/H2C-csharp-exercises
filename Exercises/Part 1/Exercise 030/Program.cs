7c5ee058ab32cbef58f571d1176fbcf34bfb61bc
Jake Reddy <Serathian@outlook.com>
2020-10-19 12:47:13 +0300

    19,10,20

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -7,7 +7,44 @@ namespace exercise_30
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("What is your grade? [0 - 100]");
+            int grade = Convert.ToInt32(Console.ReadLine());
 
+            switch (grade){
+            
+            case int n when (n < 0):
+                    Console.WriteLine("Impossible");
+                    break;
+                    
+            case int n when (n >= 0 && n <= 49):
+                    Console.WriteLine("Fail");
+                    break;
+                    
+            case int n when (n >= 50 && n <= 59):
+                    Console.WriteLine("1");
+                    break;
+                    
+            case int n when (n >= 60 && n <= 69):
+                    Console.WriteLine("2");
+                    break;
+                    
+            case int n when (n >= 70 && n <= 79):
+                    Console.WriteLine("3");
+                    break;
+                    
+            case int n when (n >= 80 && n <= 89):
+                    Console.WriteLine("4");
+                    break;
+                    
+            case int n when (n >= 90 && n <= 99):
+                   Console.WriteLine("5");
+                    break;
+                    
+            case int n when (n >= 100):
+                    Console.WriteLine("Outstanding!");
+                    break;
+                    
+            }
     }
   }
 }
