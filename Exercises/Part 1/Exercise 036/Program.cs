947680c66e6692753ba8ae718c3b8a99b89e690e
Jake Reddy <Serathian@outlook.com>
2020-10-19 13:18:42 +0300

    19,10,20

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -7,7 +7,15 @@ namespace exercise_36
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give me a number");
+            int number = Convert.ToInt32(Console.ReadLine());
 
+            if(number == 0){
+            Environment.Exit(1);}
+            else if(number <= -1){
+            Console.WriteLine("Thats negative");}
+            else{
+            Console.WriteLine($"{number*number}");}
     }
   }
 }
