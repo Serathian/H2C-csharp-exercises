8b2e8e3569737c08453e0382f795553b13798a80
Jake Reddy <Serathian@outlook.com>
2020-10-19 13:29:45 +0300

    19,10,20

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -7,7 +7,18 @@ namespace exercise_38
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      int sum = 0;
+      int number = 1;
+      int tally = 0;
+      // Write your code here:
+      while(number != sum){
+                
+      Console.WriteLine("Give me a number");
+            number = Convert.ToInt32(Console.ReadLine());
+                if(number <= -1){
+                tally++;}
+        }
+            Console.WriteLine($"Total negative numbers given: {tally}");
     }
   }
 }
