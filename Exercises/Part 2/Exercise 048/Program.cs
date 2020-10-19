5a01d4f4c0699fb9168662963799bfb5cfa4cbe8
Jake Reddy <Serathian@outlook.com>
2020-10-19 16:49:46 +0300

    19,10,20

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -8,7 +8,35 @@ namespace exercise_48
     {
 
       // Write your code here:
- 
+     int target = -1;
+     int userNumber = 1;
+     int tally = 0;
+     int sum = 0;
+     int evenNum = 0;  
+     int oddNum = 0;       
+          while(userNumber != target){
+          tally++;
+
+          Console.WriteLine("Give me a number");
+                userNumber = Convert.ToInt32(Console.ReadLine());
+                if(userNumber != -1){
+                sum = userNumber + sum;
+                if(userNumber%2 == 0){
+                    evenNum++;
+                }
+                else{
+                    oddNum++;
+                }
+                }
+            }
+                Console.WriteLine(
+                    "Thx! Bye!\n" +
+                    $"Total sum: {sum}\n" +
+                    $"Total number given: {tally}\n" +
+                    $"Average: {sum / tally}\n" +
+                    $"Even: {evenNum}\n" +
+                    $"Odd: {oddNum}\n"
+                    );
     }
   }
 }
