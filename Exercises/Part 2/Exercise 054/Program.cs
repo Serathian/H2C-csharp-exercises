26e5eb82360c2ace681280a7ceefbe07500b061b
Jake Reddy <Serathian@outlook.com>
2020-10-20 11:43:13 +0300

    20,10,20

--- Exercises/Part 2/Exercise 054/Program.cs
+++ Exercises/Part 2/Exercise 054/Program.cs
@@ -7,10 +7,18 @@ namespace exercise_54
     public static void Main(String[] args)
     {
       // Call your method here:
-
+      DivisibleByThreeInRange(3, 10);
     }
 
     // Write your method here:
-
+    public static void DivisibleByThreeInRange(int beg, int end){
+        for(int i = beg; i <= end; i++){
+            if(i%3 == 0){
+                Console.WriteLine($"{i}");
+                }
+            
+            }
+        return;
+        }
   }
 }
