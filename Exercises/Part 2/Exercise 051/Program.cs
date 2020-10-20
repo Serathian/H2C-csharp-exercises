84904453083d18a801d51f4f39d1a611efb15382
Jake Reddy <Serathian@outlook.com>
2020-10-20 11:25:16 +0300

    20,10,20

--- Exercises/Part 2/Exercise 051/Program.cs
+++ Exercises/Part 2/Exercise 051/Program.cs
@@ -7,11 +7,17 @@ namespace exercise_51
     public static void Main(String[] args)
     {
       // Call your method here:
-
+      
+      PrintUntilNumber(Convert.ToInt32(Console.ReadLine()));
 
     }
 
     // Write your method here:
-
+    public static void PrintUntilNumber(int startNum){
+            for(int i = 1; i <= startNum; i++){
+            Console.WriteLine($"{i}");
+            }
+            return;
+        }
   }
 }
