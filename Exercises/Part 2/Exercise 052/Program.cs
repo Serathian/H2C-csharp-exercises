5b584080858fd099bc67dc819002cb15a0070a1e
Jake Reddy <Serathian@outlook.com>
2020-10-20 11:28:00 +0300

    20,10,20

--- Exercises/Part 2/Exercise 052/Program.cs
+++ Exercises/Part 2/Exercise 052/Program.cs
@@ -7,10 +7,15 @@ namespace exercise_52
     public static void Main(String[] args)
     {
       // Call your method here:
-
+      PrintFromNumberToOne(Convert.ToInt32(Console.ReadLine()));
     }
 
     // Write your method here:
-
+    public static void PrintFromNumberToOne(int num){
+         for(int i = num; i >= 1; i--){
+            Console.WriteLine($"{i}");
+            }
+            return;
+        }
   }
 }
