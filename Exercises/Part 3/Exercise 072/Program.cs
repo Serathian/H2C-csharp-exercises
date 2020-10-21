d3ff27b0d19e96c36bdacf472f34a97ee700f08d
Jake Reddy <Serathian@outlook.com>
2020-10-21 13:45:04 +0300

    21,10,20

--- Exercises/Part 3/Exercise 072/Program.cs
+++ Exercises/Part 3/Exercise 072/Program.cs
@@ -7,7 +7,36 @@ namespace exercise_72
   {
     public static void Main(string[] args)
     {
-
+        List<int> list = new List<int>();
+            int currentSmallest = 0;
+      while (true)
+      {
+        int input = Convert.ToInt32(Console.ReadLine());
+        if (input == 9999)
+        {
+          break;
+        }
+        
+        list.Add(input);
+      }
+      
+      int smallest = 10000;
+            for(int i = 0; i < list.Count; i++ )
+            {
+                if(list[i] < smallest)
+                    {
+                        smallest = list[i];
+                    }
+            }
+            Console.WriteLine($"Smallest number is {smallest}");
+            for(int i = 0; i < list.Count; i++ )
+            {
+                if(list[i] == smallest)
+                {
+                Console.WriteLine($"Found at index {i}");
+                }
+            }
+        
     }
   }
 }
