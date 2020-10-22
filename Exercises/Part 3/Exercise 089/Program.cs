396c17eab408efa5883b19d968973a74f7c23f0f
Jake Reddy <Serathian@outlook.com>
2020-10-22 12:08:31 +0300

    22,10,20

--- Exercises/Part 3/Exercise 089/Program.cs
+++ Exercises/Part 3/Exercise 089/Program.cs
@@ -7,7 +7,18 @@ namespace exercise_89
   {
     public static void Main(string[] args)
     {
-
+        while(true)
+            {
+                Console.WriteLine("Enter a sentance");
+                
+                string sentance = Console.ReadLine();
+                var words = sentance.Split(' ');
+                
+                
+                
+                Console.WriteLine(words[words.Length - 1]);
+                
+            }
 
     }
   }
