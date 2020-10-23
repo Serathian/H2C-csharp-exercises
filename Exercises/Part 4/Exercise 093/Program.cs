51d7376afb36c592c4cb2ced7d669fd057445d9a
Jake Reddy <Serathian@outlook.com>
2020-10-23 14:06:45 +0300

    23,10,20 VS wasnt finding the Account class so i just copied it into the same file.

--- Exercises/Part 4/Exercise 093/Program.cs
+++ Exercises/Part 4/Exercise 093/Program.cs
@@ -8,7 +8,34 @@ namespace exercise_93
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
+    
+    Account heikkisAccount = new Account("Heikki's account", 100.00);
+    
+    heikkisAccount.Deposit(20);
+    Console.WriteLine("The balance of Heikki's other account is now: " + heikkisAccount.balance);
+    }
+  }
+    public class Account {
 
+    public double balance { get; set; }
+    private string owner;
+
+    public Account(string owner, double balance) {
+        this.balance = balance;
+        this.owner = owner;
+    }
+
+    public void Deposit(double amount) {
+        this.balance = this.balance + amount;
+    }
+
+    public void Withdrawal(double amount) {
+        this.balance = this.balance - amount;
+    }
+
+    public override string ToString()
+    {
+      return this.owner + " balance: " + this.balance;
     }
   }
 }
