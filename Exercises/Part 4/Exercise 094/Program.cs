209eaa190c22db6f93cb7584f1ace5456108c56d
Jake Reddy <Serathian@outlook.com>
2020-10-23 14:19:27 +0300

    23,10,20

--- Exercises/Part 4/Exercise 094/Program.cs
+++ Exercises/Part 4/Exercise 094/Program.cs
@@ -8,7 +8,39 @@ namespace exercise_94
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
+        Account heikkisAccount = new Account("Heikkis Account", 1000);
+        Account jakesAccount = new Account("jakes Account", 0);
+    
+        heikkisAccount.Withdrawal(100);
+        jakesAccount.Deposit(100);
 
+            Console.WriteLine(heikkisAccount.ToString());
+            Console.WriteLine(jakesAccount.ToString());
+        
+    }
+  }
+  public class Account 
+  {
+
+    public double balance { get; set; }
+    private string owner;
+
+    public Account(string owner, double balance) {
+        this.balance = balance;
+        this.owner = owner;
+    }
+
+    public void Deposit(double amount) {
+        this.balance = this.balance + amount;
+    }
+
+    public void Withdrawal(double amount) {
+        this.balance = this.balance - amount;
+    }
+
+    public override string ToString()
+    {
+      return this.owner + " balance: " + this.balance;
     }
   }
 }
