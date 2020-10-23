c7e140386c55b75d0060d1be00a01bf8f85bbcd2
Jake Reddy <Serathian@outlook.com>
2020-10-23 20:24:55 +0300

    change

--- Exercises/Part 4/Exercise 095/Program.cs
+++ Exercises/Part 4/Exercise 095/Program.cs
@@ -9,6 +9,7 @@ namespace Exercise_095
       // No need to code here.
       // Create Dog.cs and follow the instructions!
       Dog bobby = new Dog("Bobby", "Pitbull", 4);
+      Console.WriteLine("");
        //Holy shit wtf is going on
     }
   }
