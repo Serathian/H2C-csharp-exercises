76d39778ada6e46e9c38a7c0b5e0d0030bd40119
Jake Reddy <Serathian@outlook.com>
2020-10-26 19:06:09 +0200

    26,10,20

---
+++ Exercises/Part 4/Exercise 113/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercise_113
+{
+  public class Book
+  {
+    public string Name { get; set; }
+    public int NumPages { get; set; }
+    public int PubYear { get; set; }
+
+    public Book(string name = "unknown", int numPages = 0, int pubYear = 0000)
+	{
+        this.Name = name;
+        this.NumPages = numPages;
+        this.PubYear = pubYear;
+	}
+
+    public override string ToString()
+    {
+        return this.Name + ", " + this.NumPages + " pages, " + this.PubYear;
+    }
+    }
+}
--- Exercises/Part 4/Exercise 113/Program.cs
+++ Exercises/Part 4/Exercise 113/Program.cs
@@ -9,6 +9,68 @@ namespace exercise_113
     {
       // IMPLEMENT YOUR CODE IN HERE!
       // Remember also to create the file Book.cs and class in it!
+        List<Book> books = new List<Book>();
+        bool run = true;
+
+            while (run == true)
+	        {
+            Console.Write("Name:");
+            string name = Console.ReadLine();
+
+                if (name == "")
+	            {
+                    run = false;
+                    break;
+	            }
+            Console.Write("Pages:");
+            int pages = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Publication year:");
+            int pubYear = Convert.ToInt32(Console.ReadLine());
+
+            books.Add(new Book(name, pages, pubYear));
+	        }
+
+            Console.WriteLine();
+
+            Console.WriteLine(
+                "What information will be printed? \n" +
+                "1. Everything. \n" +
+                "2. Title. \n"
+                );
+            string print = Console.ReadLine().ToLower();
+            int index = 3;
+            if (print.Contains("1") | print.Contains("everything") == true) 
+            {
+                index = 1;
+            }
+            else if (print.Contains("2") | print.Contains("Title") == true)
+	        {
+                index = 2;
+	        }
+            else
+	        {
+                index = 3;
+	        }
+
+            switch (index)
+	        {
+                case 1:
+                    for (int i = 0; i < books.Count; i++)
+			        {
+                        Console.WriteLine(books[i].ToString());
+			        }
+                    break;
+                case 2:
+                    for (int i = 0; i < books.Count; i++)
+			        {
+                        Console.WriteLine(books[i].Name);  
+                        
+			        }
+                    break;
+		        default:
+                    break;
+	        }
+
 
     }
   }
