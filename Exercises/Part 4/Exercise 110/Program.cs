a26bca8bf6533989707b34fd3ea59f6fb04d2cda
Jake Reddy <Serathian@outlook.com>
2020-10-26 17:15:13 +0200

    26,10,20

--- Exercises/Part 4/Exercise 110/Program.cs
+++ Exercises/Part 4/Exercise 110/Program.cs
@@ -8,6 +8,30 @@ namespace exercise_110
     public static void Main(string[] args)
     {
       // IMPLEMENT YOUR CODE IN HERE!
+
+        List<Item> items = new List<Item>();
+        bool read = true;
+
+        while (read == true)
+	    {
+        string name = Console.ReadLine();
+                
+            if (name == "")
+	        {
+                read = false;
+                break;
+	        }
+            else
+	        {
+                items.Add(new Item(name));
+	        }
+	    }
+
+        for (int i = 0; i < items.Count; i++)
+			{
+             Console.WriteLine(items[i].ToString());
+			}
+
     }
   }
 }
