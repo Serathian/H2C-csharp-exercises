c40908e4d17a77eb9cd695684bb4b436d9c5a5d1
Jake Reddy <Serathian@outlook.com>
2020-10-26 19:16:01 +0200

    26,10,20

--- Exercises/Part 4/Exercise 115/Program.cs
+++ Exercises/Part 4/Exercise 115/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace exercise_115
 {
@@ -6,7 +7,19 @@ namespace exercise_115
   {
     public static void Main(string[] args)
     {
-
+        bool run = true;
+            
+            while (run == true)
+	        {
+            string useless = (Console.ReadLine());
+                    if (useless == "end")
+	                {
+                        run = false;
+                            break;
+	                }
+            int x = Convert.ToInt32(useless);
+            Console.WriteLine((x * x) * x);
+            }
     }
   }
 }
