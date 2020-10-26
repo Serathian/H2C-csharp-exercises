138fcfd90b6acdfcb35591618e29cdaecbe18d8c
Jake Reddy <Serathian@outlook.com>
2020-10-26 13:57:50 +0200

    26,10,20

---
+++ Exercises/Part 4/Exercise 104/Multiplier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercise_104
+{
+	public class Multiplier
+	{
+        public int Value { get; set; }
+
+        public Multiplier(int value)
+		{
+			this.Value = value;
+		}
+		
+		public int Multiply(int number)
+        {
+			int x = number * this.Value;
+			return x;
+        }
+	}
+}
