6c7b268b77a0720b1ac304150887903c0c6abf5e
Jake Reddy <Serathian@outlook.com>
2020-10-26 17:32:05 +0200

    26,10,20

--- Exercises/Part 4/Exercise 111/Program.cs
+++ Exercises/Part 4/Exercise 111/Program.cs
@@ -9,7 +9,31 @@ namespace exercise_111
     {
       // IMPLEMENT YOUR CODE IN HERE!
       List<PersonalInformation> list = new List<PersonalInformation>();
+      bool read = true;
 
+        while (read == true)
+	    {
+        Console.WriteLine("Enter first name:");
+        string firstName = Console.ReadLine();
+                if (firstName == "")
+	        {
+                read = false;
+                break;
+	        }
+        Console.WriteLine("Enter last name:");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("Enter ID number:");
+        string idNumber =Console.ReadLine();
+
+        list.Add(new PersonalInformation(firstName, lastName, idNumber));
+	    }
+
+        Console.WriteLine();
+        for (int i = 0; i < list.Count; i++)
+			{
+             Console.WriteLine($"{list[i].firstName} {list[i].lastName}");
+			}
+        
     }
   }
 }
