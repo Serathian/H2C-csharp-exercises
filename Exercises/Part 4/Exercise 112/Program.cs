b67198fd1d54bfbf0a55749e1ce6119a2bde8b5b
Jake Reddy <Serathian@outlook.com>
2020-10-26 18:16:19 +0200

    26,10,20

--- Exercises/Part 4/Exercise 112/Program.cs
+++ Exercises/Part 4/Exercise 112/Program.cs
@@ -8,7 +8,41 @@ namespace exercise_112
     public static void Main(string[] args)
     {
       // IMPLEMENT YOUR CODE IN HERE!
+    bool run = true;
+    List<TelevisionProgram> list = new List<TelevisionProgram>();
 
+        while (run == true)
+	    {
+            Console.WriteLine("Name of TV show:");
+            string name = Console.ReadLine();
+
+                if (name == "")
+	            {
+                    run = false;
+                        break;
+	            }
+
+            Console.WriteLine($"Duration of {name}:");
+            int duration = Convert.ToInt32(Console.ReadLine());
+
+            list.Add(new TelevisionProgram(name, duration));
+	    }
+
+        Console.WriteLine();
+        Console.WriteLine("Program's maximum duration?:");
+        int maxDuration = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < list.Count; i++)
+			{
+                if (list[i].duration <= maxDuration)
+	            {
+                    Console.WriteLine(list[i].ToString());
+	            }
+                else
+	            {
+
+	            }
+			}
 
     }
   }
