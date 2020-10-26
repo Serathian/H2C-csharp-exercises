831c8832c30ae89e037f56f9bf7787c87a10c263
Jake Reddy <Serathian@outlook.com>
2020-10-26 12:42:47 +0200

    26,10,20

---
+++ Exercises/Part 4/Exercise 100/Debt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Debt
+{
+    public double InitialBalance { get; set; }
+    public double InitialInterestRate { get; set; }
+
+
+    public Debt(double initialBalance, double initialInterestRate)
+	{
+        this.InitialBalance = initialBalance;
+        this.InitialInterestRate = initialInterestRate;
+	}
+
+    public void PrintBalance()
+    {
+        Console.WriteLine(this.InitialBalance);
+    }
+
+    public void WaitOneYear()
+    {
+        this.InitialBalance = this.InitialBalance * this.InitialInterestRate;
+    }
+}
