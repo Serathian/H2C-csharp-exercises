47520414bf6c2e3de536bd499b2298c82ac31e0c
Jake Reddy <Serathian@outlook.com>
2020-10-26 14:48:59 +0200

    26,10,20

---
+++ Exercises/Part 4/Exercise 108/PaymentCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace exercise_108
+{
+	public class PaymentCard
+	{
+		private double Balance;
+
+		public PaymentCard(double openingBalance)
+		{
+			this.Balance = openingBalance;
+		}
+
+        public void EatLunch()
+        {
+            double lunch = 10.6;
+			if (this.Balance >= lunch)
+            {
+                this.Balance = this.Balance - lunch;
+            }
+        }
+
+        public void DrinkCoffee()
+        {
+            double coffee = 2;
+            if (this.Balance >= coffee)
+            {
+                this.Balance = this.Balance - coffee;
+            }
+        }
+        public override string ToString()
+        {
+			return $"The card has a balance of {this.Balance} euro";
+        }
+    }
+}
