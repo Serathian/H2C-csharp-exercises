fa49898dea440a543ece554c6f9ecdb1f79457f2
Jake Reddy <Serathian@outlook.com>
2020-10-27 14:29:12 +0200

    27,10,20

---
+++ Exercises/Part 5/Exercise 123/Counter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Counter
+{
+    public int Number { get; set; }
+
+    public Counter()
+	{
+        this.Number = 0;
+	}
+
+    public Counter (int startValue)
+    {
+        this.Number = startValue;
+    }
+
+    public void Increase()
+    {
+        this.Number++;
+    }
+
+    public void IncreaseBy(int increaseBy)
+    {
+        if (increaseBy < 0)
+        {
+
+        }
+        else
+        {
+            this.Number = this.Number - increaseBy;
+        }
+    }
+
+    public void Decrease()
+    {
+        this.Number--;
+    }
+
+    public void DecreaseBy(int decreaseBy)
+    {
+        if (decreaseBy < 0)
+        {
+
+        }
+        else
+        {
+            this.Number = this.Number - decreaseBy;
+        }
+    }
+}
