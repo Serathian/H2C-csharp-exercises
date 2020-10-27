03339eb69cb52bd559ec3729842f9bf380a41227
Jake Reddy <Serathian@outlook.com>
2020-10-27 14:12:09 +0200

    27,10,20

--- Exercises/Part 5/Exercise 122/Product.cs
+++ Exercises/Part 5/Exercise 122/Product.cs
@@ -3,20 +3,38 @@ namespace exercise_122
   public class Product
   {
     // DO NOT TOUCH THESE VARIABLES
-    private string name;
-    private string location;
-    private int weight;
+    private string Name;
+    private string Location;
+    private int Weight;
 
-    // CREATE YOUR CONSTRUCTORS HERE
+        // CREATE YOUR CONSTRUCTORS HERE
+        public Product(string name)
+        {
+            this.Name = name;
+            this.Location = "shelf";
+            this.Weight = 1;
+        }
 
+        public Product(string name, string location)
+        {
+            this.Name = name;
+            this.Location = location;
+            this.Weight = 1;
+        }
 
+        public Product(string name, int weight)
+        {
+            this.Name = name;
+            this.Location = "warehouse";
+            this.Weight = weight;
+        }
 
 
 
     // DO NOT TOUCH THIS
     public override string ToString()
     {
-      return $"{this.name} ({this.weight} kg) can be found from the {this.location}.";
+      return $"{this.Name} ({this.Weight} kg) can be found from the {this.Location}.";
     }
   }
 }
