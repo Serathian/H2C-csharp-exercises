28ec2aa328f8faf89ff92ad34b61465f081e30a7
Jake Reddy <Serathian@outlook.com>
2020-10-27 11:09:36 +0200

    27,10,20

--- Exercises/Part 4/Exercise 117/Program.cs
+++ Exercises/Part 4/Exercise 117/Program.cs
@@ -8,7 +8,9 @@ namespace exercise_117
     public static void Main(string[] args)
     {
       // Ask the user for the file name and print the content of the file
-
+      Console.WriteLine("Which file should have its contents printed?");
+      string fileName = Console.ReadLine();
+            Console.WriteLine(System.IO.File.ReadAllText($@"C:\Serathian\H2C-csharp-exercises\Exercises\Part 4\Exercise 117\{fileName}"));
       
     }
   }
