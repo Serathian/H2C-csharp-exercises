8d72e495551d74cd9f16cea1d5036ec5eb36b335
Jake Reddy <Serathian@outlook.com>
2020-10-27 13:30:48 +0200

    27,10,20

---
+++ Exercises/Part 5/Exercise 119/Timer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Timer
+{
+	private int Seconds = 0;
+	private int SecondsLimit = 60;
+	private int Miliseconds = 0;
+	private int MilisecondsLimit = 100;
+
+	public Timer()
+	{
+
+	}
+
+	public void Advance()
+    {
+		this.Miliseconds++;
+        if (this.Miliseconds >= this.MilisecondsLimit)
+        {
+            this.Miliseconds = 0;
+			this.Seconds++;
+            if (this.Seconds >= this.SecondsLimit)
+            {
+				this.Seconds = 0;
+            }
+			else
+            {
+				
+            }
+        }
+		else
+        {
+			
+        }
+    }
+
+    public override string ToString()
+    {
+        return this.Seconds.ToString("00") + ":" + this.Miliseconds.ToString("00");
+    }
+}
