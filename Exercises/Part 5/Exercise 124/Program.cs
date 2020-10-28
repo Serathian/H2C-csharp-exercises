9d18c8444422d940724a76996b33f63c9c493284
Jake Reddy <Serathian@outlook.com>
2020-10-28 18:07:34 +0200

    28,10,20

--- Exercises/Part 5/Exercise 124/Program.cs
+++ Exercises/Part 5/Exercise 124/Program.cs
@@ -4,11 +4,11 @@ namespace exercise_124
 {
   class Program
   {
-    public static void Main(string[] args)
-    {
-      // Implement your program here!
-
-      
-    }
+	public static void Main(string[] args)
+	{
+	  // Implement your program here!
+	  string iAmNull = null;
+			iAmNull.ToString();
+	}
   }
 }
