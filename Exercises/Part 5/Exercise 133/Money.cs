0337b6d139b88d424387575b193c1f5512c23490
Jake Reddy <Serathian@outlook.com>
2020-10-29 15:50:51 +0200

    29,10,20

--- Exercises/Part 5/Exercise 133/Money.cs
+++ Exercises/Part 5/Exercise 133/Money.cs
@@ -20,26 +20,66 @@ namespace exercise_133
 
     public Money Plus(Money addition)
     {
-      Money newMoney = new Money(/* Do something here*/);
-      // create a new Money object that has the correct worth
+      Money newMoney = new Money(0, 0);
+            // create a new Money object that has the correct worth
+            newMoney.cents = this.cents + addition.cents;
+            newMoney.euros = this.euros + addition.euros;
 
-      // return the new Money object
-      return newMoney;
+            if (newMoney.cents > 99)
+            {
+                newMoney.euros = newMoney.euros + newMoney.cents / 100;
+                newMoney.cents = newMoney.cents % 100;
+            }
+            // return the new Money object
+            return newMoney;
     }
 
     public Money Minus(Money decreaser)
     {
-      Money newMoney = new Money(/* Do something here*/);
-      // create a new Money object that has the correct worth
+      Money newMoney = new Money(0, 0);
+            // create a new Money object that has the correct worth
+            newMoney.cents = this.cents - decreaser.cents;
+            newMoney.euros = this.euros - decreaser.euros;
 
-      // return the new Money object
-      return newMoney;
+            if (newMoney.cents > 99)
+            {
+                newMoney.euros = newMoney.euros - newMoney.cents / 100;
+                newMoney.cents = newMoney.cents % 100;
+            }
+            if(newMoney.euros < 0)
+            {
+                newMoney.euros = 0;
+            }
+            if(newMoney.cents < 0)
+            {
+                newMoney.cents = 0;
+            }
+            // return the new Money object
+            return newMoney;
     }
 
     public bool LessThan(Money compared)
     {
       // Do something here
-      return false;
+        if(this.euros > compared.euros)
+        {
+                return false;
+        }
+        else if (this.euros == compared.euros)
+        {
+            if (this.cents > compared.cents)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else
+        {
+                return true;
+        }
     }
 
     public override string ToString()
