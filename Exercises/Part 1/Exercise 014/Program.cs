3a34629aacde132e13cbc73ddf213784b61cd2f0
Jake Reddy <Serathian@outlook.com>
2020-10-29 21:56:16 +0200

    29,10,20 little update while helping Pia

--- Exercises/Part 1/Exercise 014/Program.cs
+++ Exercises/Part 1/Exercise 014/Program.cs
@@ -8,7 +8,29 @@ namespace exercise_14
     {
       Console.WriteLine("Give me the truth!");
       // Write your code here:
+
+    //Option 1
      Console.WriteLine($"{Console.ReadLine()}");
+            
+    //Option 2, we hope the user enters "True" or "False"
+    string reply = Console.ReadLine();
+    bool replyBool = Convert.ToBoolean(reply);
+    Console.WriteLine(replyBool);
+
+    //Option 3.
+    string userReply = Console.ReadLine().ToLower();
+    if (userReply == "true")
+	{
+                Console.WriteLine("True")
+	}
+    else if (userReply == "false")
+	{
+                Console.WriteLine("False")
+	}
+    else 
+    {
+                Console.WriteLine("Input not valid.")
+    }
     }
   }
 }
